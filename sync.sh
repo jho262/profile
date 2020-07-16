2fd5ba19f955b82c7e3d1fe02da188f0ea659d4f
James Ho <jsb262@gmail.com>
2020-07-16 07:46:58 -0700

    fix bug when syncing files

--- sync.sh
+++ sync.sh
@@ -7,9 +7,11 @@
 ## named index.html), this generated list of files to be sync'd will need to be cross-referenced with a list of files that
 ## are referenced by the parent html to determine the actual files that must be sync'd.
 
+WRKDIR=`pwd`
+BASE_DIRNAME=`basename $WRKDIR`
 
 ## Create remote QA file checksums and flist 
-ssh -i ~/.ssh/mac_id_rsa ar4jf2nz@jhwebex.com "find /home/ar4jf2nz/public_html/QA -type f | grep -v '\/old\/' | while read f;do cksum \$f | awk '{print \$NF, \$1}' | sed 's#/home/ar4jf2nz/public_html/qa##';done" | sort > remote.cksum
+ssh -i ~/.ssh/mac_id_rsa ar4jf2nz@jhwebex.com "find /home/ar4jf2nz/public_html/QA_${BASE_DIRNAME} -type f | grep -v '\/old\/' | while read f;do cksum \$f | awk '{print \$NF, \$1}' | sed 's#/home/ar4jf2nz/public_html/QA_${BASE_DIRNAME}##';done" | sort > remote.cksum
 cat remote.cksum | awk '{print $1}' | sort > remote.flist
 
 
