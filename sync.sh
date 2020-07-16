805cb6c46454a0a16a45f35f2db7835dbeebdb2a
James Ho <jsb262@gmail.com>
2020-07-16 08:33:33 -0700

    fix bug in sync.sh, remove leading / from files in remote.cksum

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ WRKDIR=`pwd`
 BASE_DIRNAME=`basename $WRKDIR`
 
 ## Create remote QA file checksums and flist 
-ssh -i ~/.ssh/mac_id_rsa ar4jf2nz@jhwebex.com "find /home/ar4jf2nz/public_html/QA_${BASE_DIRNAME} -type f | grep -v '\/old\/' | while read f;do cksum \$f | awk '{print \$NF, \$1}' | sed 's#/home/ar4jf2nz/public_html/QA_${BASE_DIRNAME}##';done" | sort > remote.cksum
+ssh -i ~/.ssh/mac_id_rsa ar4jf2nz@jhwebex.com "find /home/ar4jf2nz/public_html/QA_${BASE_DIRNAME} -type f | grep -v '\/old\/' | while read f;do cksum \$f | awk '{print \$NF, \$1}' | sed 's#/home/ar4jf2nz/public_html/QA_${BASE_DIRNAME}/##';done" | sort > remote.cksum
 cat remote.cksum | awk '{print $1}' | sort > remote.flist
 
 
@@ -21,7 +21,7 @@ cat local.cksum | awk '{print $1}' | sort > local.flist
 
 
 ## Generate list of files that need to be sync'd 
-comm -23 local.cksum remote.cksum | awk '{print $1}' > sync.flist
+comm -23 local.cksum remote.cksum | awk '{print $1}' | sort -u > sync.flist
 comm -23 local.flist remote.flist >> sync.flist
 
 
